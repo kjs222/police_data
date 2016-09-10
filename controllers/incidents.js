8b5ee6f21402c949914f8016c97c5dbae4312d4e
Kerry <kerry.sheldon@gmail.com>
2016-09-09 19:16:15 -0600

    adds serialization and eager loading to incidents

--- controllers/incidents.js
+++ controllers/incidents.js
@@ -1,7 +1,10 @@
 var models  = require('../models');
 
 exports.getIncidents = function(req, res) {
-  models.Incident.findAll({limit: 100}).then(function(incidents) {
-    return res.json(incidents)
+  models.Incident.findAll({limit: 100, include: [ models.Beat, models.Disposition, models.CallType ]}).then(function(incidents) {
+    for(i in incidents) {
+      incidents[i] = incidents[i].serialize();
+    }
+    return res.json(incidents);
   });
 };
--- models/incident.js
+++ models/incident.js
@@ -1,4 +1,5 @@
 'use strict';
+
 module.exports = function(sequelize, DataTypes) {
   var Incident = sequelize.define('Incident', {
     number: DataTypes.STRING,
@@ -12,6 +13,32 @@ module.exports = function(sequelize, DataTypes) {
     street_type2: DataTypes.STRING,
     priority: DataTypes.STRING
   }, {
+    instanceMethods: {
+      formatAddress: function() {
+        var formatted = this.street_number + " " + this.street + " " + this.street_type
+        formatted = formatted.replace(" NA ", " ")
+        if(formatted.slice(-2) === "NA") {
+          formatted = formatted.slice(0,-3);
+        }
+        if(formatted.slice(0, 2) === "NA ") {
+          formatted = formatted.slice(3);
+        }
+        return formatted;
+      },
+      serialize: function() {
+        return {"incident number": this.number,
+                "date": this.date,
+                "address": this.formatAddress(),
+                "priority": this.priority,
+                "beat": this.Beat.number,
+                "neighborhood": this.Beat.neighborhood,
+                "disposition code": this.Disposition.code,
+                "disposition description": this.Disposition.description,
+                "call type code": this.CallType.code,
+                "call type description": this.CallType.description
+              }
+      }
+    },
     classMethods: {
       associate: function(models) {
         Incident.belongsTo(models.CallType, {
