2bb7dfa09ffba3ab5624244c1d5b4fd16996d2f3
Kerry <kerry.sheldon@gmail.com>
2016-09-12 13:54:35 -0600

    adds single method to handle search and all incidents
    -search terms are designed to be more user friendly
    -invalid search parameters are handled

--- controllers/incidents.js
+++ controllers/incidents.js
@@ -2,31 +2,44 @@ var models  = require('../models');
 const querystring = require('querystring');
 
 exports.getIncidents = function(req, res) {
-  if(Object.keys(req.query).length > 0) {
-    searchResults(req, res);
-  } else {
-    allIncidents(req, res)
-  };
+  allIncidents(req, res)
 };
 
-var searchResults = function(req, res) {
-  models.Incident.findAll({where: req.query, limit: 100, include: [ models.Beat, models.Disposition, models.CallType ]}).then(function(incidents) {
-    for(i in incidents) {
-      incidents[i] = incidents[i].serialize();
-    }
-    return res.json(incidents);
-  });
-}
 
 var allIncidents = function(req, res) {
-  // maybe move this top stuff to model - which returns incidents
-  // and this just does the json part?
-  models.Incident.findAll({where: req.query, limit: 100, include: [ models.Beat, models.Disposition, models.CallType ]}).then(function(incidents) {
-    for(i in incidents) {
-      incidents[i] = incidents[i].serialize();
-    }
-    return res.json(incidents);
-  });
+  search_terms = mapQuery(req.query)
+  if (search_terms["invalid_parameter"]) {
+    return res.json({invalid_request: "unrecognized search parameter"})
+  } else {
+    models.Incident.findAll({where: mapQuery(req.query), limit: 100, include: [ models.Beat, models.Disposition, models.CallType ]}).then(function(incidents) {
+      for(i in incidents) {
+        incidents[i] = incidents[i].serialize();
+      }
+      return res.json(incidents);
+    });
+  };
 }
 
-// {beat_number: Beat.number, ad}
+var queryHashMap = {number: "number",
+                    priority: "priority",
+                    beat: "Beat.number",
+                    neighborhood: "Beat.neighborhood",
+                    disp_code: "Disposition.code",
+                    disp_desc: "Disposition.description",
+                    call_code: "CallType.code",
+                    call_desc: "CallType.description",
+                    street: "street",
+                    street_number: "street_number",
+                    street_dir: "street_dir"};
+
+var mapQuery = function(query) {
+  var newQuery = {}
+  for(var searchItem in query) {
+    if(!queryHashMap[searchItem]) {
+      newQuery["invalid_parameter"] = query[searchItem];
+    } else {
+      newQuery[queryHashMap[searchItem]] = query[searchItem];
+    };
+  };
+  return newQuery;
+};
