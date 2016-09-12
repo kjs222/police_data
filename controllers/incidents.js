99e52139ea6152b8bbb5705f4e2a9e68b2f5124a
Kerry <kerry.sheldon@gmail.com>
2016-09-12 12:40:35 -0600

    search working with separate search and all methods; to consolidate to single function

--- controllers/incidents.js
+++ controllers/incidents.js
@@ -1,17 +1,32 @@
 var models  = require('../models');
 const querystring = require('querystring');
 
-
-
 exports.getIncidents = function(req, res) {
-  if(Object.keys(req.query).length > 0 && req.query.constructor === Object) {
-    return res.json(req.query)
+  if(Object.keys(req.query).length > 0) {
+    searchResults(req, res);
   } else {
-    models.Incident.findAll({limit: 100, include: [ models.Beat, models.Disposition, models.CallType ]}).then(function(incidents) {
-      for(i in incidents) {
-        incidents[i] = incidents[i].serialize();
-      }
-      return res.json(incidents);
-    });
+    allIncidents(req, res)
   };
 };
+
+var searchResults = function(req, res) {
+  models.Incident.findAll({where: req.query, limit: 100, include: [ models.Beat, models.Disposition, models.CallType ]}).then(function(incidents) {
+    for(i in incidents) {
+      incidents[i] = incidents[i].serialize();
+    }
+    return res.json(incidents);
+  });
+}
+
+var allIncidents = function(req, res) {
+  // maybe move this top stuff to model - which returns incidents
+  // and this just does the json part?
+  models.Incident.findAll({where: req.query, limit: 100, include: [ models.Beat, models.Disposition, models.CallType ]}).then(function(incidents) {
+    for(i in incidents) {
+      incidents[i] = incidents[i].serialize();
+    }
+    return res.json(incidents);
+  });
+}
+
+// {beat_number: Beat.number, ad}
