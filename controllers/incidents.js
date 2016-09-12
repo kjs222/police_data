bd1419d8becda7434c68eebcdf9fa0c257176678
Kerry <kerry.sheldon@gmail.com>
2016-09-12 08:38:43 -0600

    starts search on incidents

--- controllers/incidents.js
+++ controllers/incidents.js
@@ -1,10 +1,17 @@
 var models  = require('../models');
+const querystring = require('querystring');
+
+
 
 exports.getIncidents = function(req, res) {
-  models.Incident.findAll({limit: 100, include: [ models.Beat, models.Disposition, models.CallType ]}).then(function(incidents) {
-    for(i in incidents) {
-      incidents[i] = incidents[i].serialize();
-    }
-    return res.json(incidents);
-  });
+  if(Object.keys(req.query).length > 0 && req.query.constructor === Object) {
+    return res.json(req.query)
+  } else {
+    models.Incident.findAll({limit: 100, include: [ models.Beat, models.Disposition, models.CallType ]}).then(function(incidents) {
+      for(i in incidents) {
+        incidents[i] = incidents[i].serialize();
+      }
+      return res.json(incidents);
+    });
+  };
 };
