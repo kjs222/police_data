ec2d12947476320023aa2250827bf62f0d6be5e9
Kerry <kerry.sheldon@gmail.com>
2016-10-24 10:06:54 -0600

    ties javascript assets to frontend

---
+++ public/javascripts/index.js
@@ -0,0 +1,15 @@
+
+document.addEventListener('DOMContentLoaded', function(){
+  var but = document.getElementById("myButton")
+  but.addEventListener('click', function(){
+    console.log("clicked");
+  });
+});
+
+
+console.log('file included');
+
+// var but = document.getElementById("myButton")
+// but.addEventListener('click', function(){
+//   console.log("clicked");
+// });
