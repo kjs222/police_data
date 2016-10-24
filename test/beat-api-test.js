3211ad3809b4d79d15b831e0761899526d53b689
Kerry <kerry.sheldon@gmail.com>
2016-10-24 15:59:46 -0600

    adds api tests for calltypes, dispositions, and beats endpoints

---
+++ test/beat-api-test.js
@@ -0,0 +1,61 @@
+const chai = require('chai');
+const assert = chai.assert;
+const request = require('request');
+const app = require('../app');
+const routes = require('../routes/index');
+
+
+describe('Beat API', () => {
+
+  before((done) => {
+    this.port = 9876;
+
+    this.server = app.listen(this.port, (err, result) => {
+      if (err) { return done(err); }
+      done();
+    });
+
+    this.request = request.defaults({
+      baseUrl: 'http://localhost:9876/'
+    });
+  });
+
+  after(() => {
+    this.server.close();
+  });
+
+  describe('GET /api/v1/beats', () => {
+    it('should return an array of beats', (done) => {
+      this.request.get('/api/v1/beats', (error, response) => {
+        if (error) { done(error); }
+        var parsed = JSON.parse(response.body)
+        assert.isArray(parsed);
+        done();
+      });
+    });
+
+    it('should return 136 beats', (done) => {
+      this.request.get('/api/v1/beats', (error, response) => {
+        if (error) { done(error); }
+        var parsed = JSON.parse(response.body)
+        assert.equal(parsed.length, 136);
+        done();
+      });
+    });
+
+    it('should return beats with correct keys', (done) => {
+      this.request.get('/api/v1/beats', (error, response) => {
+        if (error) { done(error); }
+        var parsedBeat = JSON.parse(response.body)[0]
+        var keys = Object.keys(parsedBeat)
+        assert.isObject(parsedBeat);
+        assert.equal(keys.length, 3);
+        assert.include(keys, "neighborhood");
+        assert.include(keys, "number");
+        done();
+      });
+    });
+
+  });
+
+});
---
+++ test/disposition-api-test.js
@@ -0,0 +1,61 @@
+const chai = require('chai');
+const assert = chai.assert;
+const request = require('request');
+const app = require('../app');
+const routes = require('../routes/index');
+
+
+describe('Disposition API', () => {
+
+  before((done) => {
+    this.port = 9876;
+
+    this.server = app.listen(this.port, (err, result) => {
+      if (err) { return done(err); }
+      done();
+    });
+
+    this.request = request.defaults({
+      baseUrl: 'http://localhost:9876/'
+    });
+  });
+
+  after(() => {
+    this.server.close();
+  });
+
+  describe('GET /api/v1/dispositions', () => {
+    it('should return an array of dispositions', (done) => {
+      this.request.get('/api/v1/dispositions', (error, response) => {
+        if (error) { done(error); }
+        var parsed = JSON.parse(response.body)
+        assert.isArray(parsed);
+        done();
+      });
+    });
+
+    it('should return 42 dispositions', (done) => {
+      this.request.get('/api/v1/dispositions', (error, response) => {
+        if (error) { done(error); }
+        var parsed = JSON.parse(response.body)
+        assert.equal(parsed.length, 42);
+        done();
+      });
+    });
+
+    it('should return dispositions with correct keys', (done) => {
+      this.request.get('/api/v1/dispositions', (error, response) => {
+        if (error) { done(error); }
+        var parsedDisposition = JSON.parse(response.body)[0]
+        var keys = Object.keys(parsedDisposition)
+        assert.isObject(parsedDisposition);
+        assert.equal(keys.length, 3);
+        assert.include(keys, "code");
+        assert.include(keys, "description");
+        done();
+      });
+    });
+
+  });
+
+});
