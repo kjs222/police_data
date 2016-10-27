00511c869d0f74b5f3f3ffc308e08736f340b36c
Kerry <kerry.sheldon@gmail.com>
2016-10-27 13:58:40 -0600

    adds documentation for all endpoints using swagger

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,11 @@ var express = require('express');
 var router = express.Router();
 var pg = require('pg');
 
-
 var callTypesController = require('../controllers/calltypes');
 var beatsController = require('../controllers/beats');
 var dispositionsController = require('../controllers/dispositions');
 var incidentsController = require('../controllers/incidents');
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
@@ -17,13 +15,6 @@ router.get('/', function(req, res, next) {
 router.get('/documentation', function(req, res, next) {
   res.sendfile('public/dist/index.html');
 });
-
-module.exports = router;
-
-// // Create endpoint handlers for indiv items /items/:item_id
-// router.route('/items/:item_id')
-//   .get(itemController.getItem)
-
 
 router.route('/api/v1/call_types')
   .get(callTypesController.getCallTypes);
@@ -36,6 +27,8 @@ router.route('/api/v1/beats')
 
 router.route('/api/v1/incidents')
   .get(incidentsController.getIncidents);
+
+module.exports = router;
 
 if (!module.parent) {
   app.listen(app.get('port'), () => {
