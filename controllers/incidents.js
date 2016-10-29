df5b0a566641337f619ad3f0e40f3d3d722f93d9
Kerry <kerry.sheldon@gmail.com>
2016-10-29 11:13:22 -0600

    adds total num results to json response

--- controllers/incidents.js
+++ controllers/incidents.js
@@ -5,54 +5,52 @@ exports.getIncidents = function(req, res) {
   allIncidents(req, res)
 };
 
-
 var allIncidents = function(req, res) {
   var page = req.query["page"] || 1;
   var offset = 100 * (page - 1);
-  var searchQuery = mapQuery(req.query);
+  var searchQuery = transformQuery(req.query);
 
   if (searchQuery["invalid_parameter"]) {
     return res.json({invalid_request: "unrecognized search parameter"})
   } else {
-    models.Incident.findAll({where: searchQuery, offset: offset, limit: 100, include: [ models.Beat, models.Disposition, models.CallType ]}).then(function(incidents) {
-      for(i in incidents) {
-        incidents[i] = incidents[i].serialize();
-      }
-      return res.json(incidents);
+    models.Incident.findAndCountAll({where: searchQuery, offset: offset, limit: 100, include: [ models.Beat, models.Disposition, models.CallType ]}).then(function(results) {
+      customizeJsonKeys(results);
+      serializeIncidents(results);
+      return res.json(results);
     });
   };
 }
 
-var queryHashMap = {number: "number",
-                    priority: "priority",
-                    beat: "Beat.number",
-                    neighborhood: "Beat.neighborhood",
-                    disp_code: "Disposition.code",
-                    disp_desc: "Disposition.description",
-                    call_code: "CallType.code",
-                    call_desc: "CallType.description",
-                    street: "street",
-                    street_number: "street_number",
-                    street_dir: "street_dir",
-                    date: "date",
-                    start_date: "date",
-                    end_date: "date",
-                    page: "page"};
+var queryHashMap = {number:         "number",
+                    priority:       "priority",
+                    beat:           "Beat.number",
+                    neighborhood:   "Beat.neighborhood",
+                    disp_code:      "Disposition.code",
+                    disp_desc:      "Disposition.description",
+                    call_code:      "CallType.code",
+                    call_desc:      "CallType.description",
+                    street:         "street",
+                    street_number:  "street_number",
+                    street_dir:     "street_dir",
+                    date:           "date",
+                    start_date:     "date",
+                    end_date:       "date",
+                    page:           "page"};
 
-var mapQuery = function(query) {
+var transformQuery = function(query) {
   var newQuery = {}
   for(var searchItem in query) {
     if(queryHashMap[searchItem] === "page") {
       continue;
     } else if(!queryHashMap[searchItem]) {
       newQuery["invalid_parameter"] = query[searchItem];
-    } else if (searchItem == "date") {
+    } else if (searchItem === "date") {
       var start = query[searchItem] + "T00:00:00.000Z"
       var end = query[searchItem] + "T23:59:59.000Z"
       newQuery[queryHashMap[searchItem]] = { between: [start, end]}
-    } else if (searchItem == "start_date"){
+    } else if (searchItem === "start_date"){
       newQuery[queryHashMap[searchItem]] = { gte: new Date(query[searchItem])}
-    } else if (searchItem == "end_date"){
+    } else if (searchItem === "end_date"){
       newQuery[queryHashMap[searchItem]] = { lt: new Date(query[searchItem])}
     } else {
       newQuery[queryHashMap[searchItem]] = query[searchItem];
@@ -60,3 +58,19 @@ var mapQuery = function(query) {
   };
   return newQuery;
 };
+
+var customizeJsonKeys = function(results) {
+  var count = results[0];
+  var incidents = results[1];
+  results.total_incidents = results.count;
+  results.incidents = results.rows;
+  delete results.count;
+  delete results.rows;
+  return results;
+}
+
+var serializeIncidents = function(results) {
+  for(i in results["incidents"]) {
+    results["incidents"][i] = results["incidents"][i].serialize();
+  }
+}
