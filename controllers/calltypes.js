836f2e0757460429c16bca13dcbb1356fc660d49
Kerry <kerry.sheldon@gmail.com>
2016-10-29 17:36:26 -0600

    adds ordering and serialization to all models

--- controllers/calltypes.js
+++ controllers/calltypes.js
@@ -1,7 +1,10 @@
 var models  = require('../models');
 
 exports.getCallTypes = function(req, res) {
-  models.CallType.findAll().then(function(callTypes) {
+  models.CallType.findAll({order: '"code" ASC'}).then(function(callTypes) {
+    for(c in callTypes) {
+      callTypes[c] = {code: callTypes[c].code, description: callTypes[c].description}
+    };
     return res.json(callTypes)
   });
 };
