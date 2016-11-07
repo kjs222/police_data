7b1e15dbe0929bed38054ee59ebe9f533cbe3cdb
Kerry <kerry.sheldon@gmail.com>
2016-11-06 20:23:13 -0700

    second d3 chart in progress

--- public/javascripts/d3_chart.js
+++ public/javascripts/d3_chart.js
@@ -1,64 +1,59 @@
-// var queryString = "?neighborhood=Talmadge&month=01/01/15&code=%A";
-// $.get('/api/v1/stats/neigh_incident_stats' + queryString, function(data) {
-//
-//   var width = 1000,
-//     height = 400,
-//     padding = 100;
-//
-//   var timeFormat = d3.time.format('%d %b %Y %H:%M')
-//
-//   var categories = data.map(function(d) { return d["call type description"] });
-//
-//
-//   var vis = d3.select(".d3-chart")
-//               .append("svg")
-//               .attr("width", width)
-//               .attr("height", height);
-//
-//   var xScale = d3.time.scale()
-//       .domain([d3.min(data, function(d) { return  timeFormat.parse(d["date"]) }), d3.max(data, function(d) { return timeFormat.parse(d["date"])})])
-//       .range([padding, width-padding*2]);
-//
-//   var yScale = d3.scale.ordinal()
-//       .domain(categories)
-//       .range([height-padding, 0]);
-//
-//   var yAxis = d3.svg.axis()
-//         .orient("left")
-//         .scale(yScale);
-//
-//   var xAxis = d3.svg.axis()
-//         .orient("bottom")
-//         .scale(xScale);
-//
-//   vis.append("g")
-//     .attr("class", "yaxis")
-//     .attr("transform", "translate("+padding+",0)")
-//     .call(yAxis);
-//
-//       // draw x axis with labels and move to the bottom of the chart area
-//   vis.append("g")
-//       .attr("class", "xaxis")
-//       .attr("transform", "translate(0," + (height - padding) + ")")
-//       .call(xAxis);
-//
-//   d3.selectAll(".dot")
-//     .data(data)
-//     .enter()
-//     .append("circle")
-//       .attr("class", "dot")
-//       .attr('cx', function(d) { debugger; return xScale[timeFormat.parse(d["date"])]  })
-//       .attr('cy', function(d) { return yScale[d['call type description']]})
-//       .attr('radius', "10px")
-//       .attr('fill', 'red')
-//
-//
-//   // vis.selectAll(".xaxis text")  // select all the text elements for the xaxis
-//   //         .attr("transform", function(d) {
-//   //             return "translate(" + this.getBBox().height*-2 + "," + this.getBBox().height + ")rotate(-45)";
-//   //       });
-//
-// });
+var queryString = "?neighborhood=Talmadge&month=01/01/15&code=%A";
+$.get('/api/v1/stats/neigh_incident_stats' + queryString, function(data) {
+
+  var width = 1000,
+    height = 400,
+    padding = 100;
+
+  var timeFormat = d3.time.format('%d %b %Y %H:%M')
+
+  var xValue = function(d) { return timeFormat.parse(d.date);}
+
+  var vis = d3.select(".d3-chart-scatter")
+              .append("svg")
+              .attr("width", width)
+              .attr("height", height);
+
+  var xScale = d3.time.scale()
+      .domain([d3.min(data, xValue), d3.max(data, xValue)])
+      .range([padding, width-padding*2]);
+
+  var yScale = d3.scale.ordinal()
+      .domain(data.map(function(d) { return d["call type code"]; }))
+      .rangePoints([height-padding - 10, padding])
+
+  var xMap = function(d) { return xScale(xValue(d));};
+
+  var yAxis = d3.svg.axis()
+        .orient("left")
+        .scale(yScale);
+
+  var xAxis = d3.svg.axis()
+        .orient("bottom")
+        .scale(xScale);
+
+  vis.append("g")
+    .attr("class", "yaxis")
+    .attr("transform", "translate("+padding+",0)")
+    .call(yAxis);
+
+      // draw x axis with labels and move to the bottom of the chart area
+  vis.append("g")
+      .attr("class", "xaxis")
+      .attr("transform", "translate(0," + (height - padding) + ")")
+      .call(xAxis);
+
+  vis.selectAll(".dot")
+    .data(data)
+    .enter()
+    .append("circle")
+      .attr("class", "dot")
+      .attr('cx', xMap)
+      .attr('cy', function(d) { return yScale(d["call type code"]); })
+      .attr('r', 5)
+      .attr('fill', 'red')
+
+});
 
 $.get('/api/v1/stats/overview_stats', function(data) {
 
@@ -67,10 +62,16 @@ $.get('/api/v1/stats/overview_stats', function(data) {
     height = 600,
     padding = 100;
 
+  var cValue = function(d) { return d.neighborhood;}
+  var color = d3.scale.category20();
+
+
   var xValue = function(d) { return d.num_incidents;}
   var yValue = function(d) { return d.num_arrests;}
+  var zValue = function(d) { return d.num_transient_incidents;}
 
-  var vis = d3.select(".d3-chart")
+
+  var vis = d3.select(".d3-chart-bubble")
               .append("svg")
               .attr("width", width)
               .attr("height", height);
@@ -83,8 +84,18 @@ $.get('/api/v1/stats/overview_stats', function(data) {
       .domain([0, d3.max(data, yValue)])
       .range([height-padding, 0]);
 
+  var zScale = d3.scale.linear()
+      .domain([0, d3.max(data, zValue)])
+      .range([3, 30]);
+
   var xMap = function(d) { return xScale(xValue(d));};
   var yMap = function(d) { return yScale(yValue(d));};
+  var zMap = function(d) { return zScale(zValue(d));};
+
+  // add the tooltip area to the webpage
+  var tooltip = d3.select("body").append("div")
+      .attr("class", "tooltip")
+      .style("opacity", 0);
 
   var yAxis = d3.svg.axis()
         .orient("left")
@@ -112,13 +123,44 @@ $.get('/api/v1/stats/overview_stats', function(data) {
       .attr("class", "dot")
       .attr('cx', xMap)
       .attr('cy', yMap)
-      .attr('r', 10)
-      .attr('fill', 'red')
+      .attr('r', zMap)
+      .attr('fill', function(d) { return color(cValue(d));})
+      .on("mouseover", function(d) {
+          tooltip.transition()
+               .duration(200)
+               .style("opacity", .9);
+          tooltip.html(d["neighborhood"] + "<br/>Total Incidents: " + xValue(d) + "<br>Total Arrests: " + yValue(d) + "<br>Total Transient Incidents: " + zValue(d))
+               .style("left", (d3.event.pageX + 5) + "px")
+               .style("top", (d3.event.pageY - 28) + "px");
+      })
+      .on("mouseout", function(d) {
+          tooltip.transition()
+               .duration(500)
+               .style("opacity", 0);
+      });
 
+      // draw legend
+var legend = vis.selectAll(".legend")
+    .data(color.domain())
+  .enter().append("g")
+    .attr("class", "legend")
+    .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
 
-  // vis.selectAll(".xaxis text")  // select all the text elements for the xaxis
-  //         .attr("transform", function(d) {
-  //             return "translate(" + this.getBBox().height*-2 + "," + this.getBBox().height + ")rotate(-45)";
-  //       });
+// draw legend colored rectangles
+legend.append("rect")
+    .attr("x", width - 18)
+    .attr("width", 18)
+    .attr("height", 18)
+    .style("fill", color);
+
+// draw legend text
+legend.append("text")
+    .attr("x", width - 24)
+    .attr("y", 9)
+    .attr("dy", ".35em")
+    .style("text-anchor", "end")
+    .text(function(d) { return d;})
 
 });
+
+// http://bl.ocks.org/weiglemc/6185069
