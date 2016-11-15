35a6e27c1ce813de836b45780d35f69c0142bfa3
Kerry <kerry.sheldon@gmail.com>
2016-11-15 16:16:07 -0700

    shell of time day scatter chart complete

--- public/javascripts/d3_chart.js
+++ public/javascripts/d3_chart.js
@@ -1,9 +1,13 @@
 var queryString = "?neighborhood=Talmadge&month=01/01/15&code=%A";
 $.get('/api/v1/stats/neigh_incident_stats' + queryString, function(data) {
+  console.log("called")
 
   var width = 1000,
-    height = 400,
+    height = 600,
     padding = 100;
+
+    var dayFormat = d3.time.format('%d %b %Y')
+    var timeFormat = d3.time.format('%Y-%m-%d %H:%M')
 
   data.forEach(function (d) {
     d["Day"] = d["date"].substring(0, d["date"].length - 6);
@@ -11,12 +15,13 @@ $.get('/api/v1/stats/neigh_incident_stats' + queryString, function(data) {
         "2000-01-01 " + d["date"].substring(d["date"].length - 5);
   }, this);
 
-  var dayFormat = d3.time.format('%d %b %Y')
-  var timeFormat = d3.time.format('%Y-%m-%d %H:%M')
+
 
   var xValue = function(d) { return dayFormat.parse(d["Day"]);}
   var yValue = function(d) { return timeFormat.parse(d["Time of Day"]);}
-  //above isnt really doing what i want
+
+  var cValue = function(d) { return d["call type description"]; }
+  var color = d3.scale.category20();
 
   var vis = d3.select(".d3-chart-time-scatter")
               .append("svg")
@@ -27,31 +32,47 @@ $.get('/api/v1/stats/neigh_incident_stats' + queryString, function(data) {
       .domain([d3.min(data, xValue), d3.max(data, xValue)])
       .range([padding, width-padding*2]);
 
-  var yScale = d3.scale.ordinal()
+  var yScale = d3.time.scale()
       .domain([d3.min(data, yValue), d3.max(data, yValue)])
-      .range([height-padding*2, padding]
-  var xMap = function(d) { return xScale(xValue(d));};
-  var yMap = function(d) { return xScale(yValue(d));};
+      .range([height-padding, padding])
 
+  var xMap = function(d) { return xScale(xValue(d));};
+  var yMap = function(d) { return yScale(yValue(d));};
+
+  var tooltip = d3.select("body").append("div")
+      .attr("class", "tooltip")
+      .style("opacity", 0);
 
   var yAxis = d3.svg.axis()
         .orient("left")
-        .scale(yScale);
+        .scale(yScale)
+        .ticks(24)
+        .tickFormat(d3.time.format("%X"));
 
   var xAxis = d3.svg.axis()
         .orient("bottom")
         .scale(xScale);
 
+
   vis.append("g")
     .attr("class", "yaxis")
     .attr("transform", "translate("+padding+",0)")
-    .call(yAxis);
+    .call(yAxis)
+    .selectAll("text")
+    .style("font-size", "10px")
 
       // draw x axis with labels and move to the bottom of the chart area
   vis.append("g")
       .attr("class", "xaxis")
       .attr("transform", "translate(0," + (height - padding) + ")")
-      .call(xAxis);
+      .call(xAxis)
+      .selectAll("text")
+      .attr("y", 15)
+      .attr("x", 0)
+      .attr("dy", ".35em")
+      .attr("transform", "rotate(-45)")
+      .style("text-anchor", "end")
+      .style("font-size","12px")
 
   vis.selectAll(".dot")
     .data(data)
@@ -61,7 +82,20 @@ $.get('/api/v1/stats/neigh_incident_stats' + queryString, function(data) {
       .attr('cx', xMap)
       .attr('cy', yMap)
       .attr('r', 5)
-      .attr('fill', 'blue')
+      .attr('fill', function(d) { return color(cValue(d)); })
+      .on("mouseover", function(d) {
+          tooltip.transition()
+               .duration(200)
+               .style("opacity", .9);
+          tooltip.html("Incident Number:" + d["incident number"] + "<br/>Date: " + d["date"] + "<br>Address: " + d["address"] + "<br>Disposition: " + d["disposition description"] + "<br>Call Type: " + d["call type description"])
+               .style("left", (d3.event.pageX + 5) + "px")
+               .style("top", (d3.event.pageY - 28) + "px");
+      })
+      .on("mouseout", function(d) {
+          tooltip.transition()
+               .duration(500)
+               .style("opacity", 0);
+      });
 
 });
 
@@ -76,9 +110,10 @@ $.get('/api/v1/stats/neigh_incident_stats' + queryString, function(data) {
 var queryString = "?neighborhood=Talmadge&month=01/01/15&code=%A";
 $.get('/api/v1/stats/neigh_incident_stats' + queryString, function(data) {
 
-  var width = 1000,
+  var width = 1200,
     height = 400,
-    padding = 100;
+    xPadding = 200,
+    yPadding = 50;
 
   var timeFormat = d3.time.format('%d %b %Y %H:%M')
 
@@ -91,11 +126,11 @@ $.get('/api/v1/stats/neigh_incident_stats' + queryString, function(data) {
 
   var xScale = d3.time.scale()
       .domain([d3.min(data, xValue), d3.max(data, xValue)])
-      .range([padding, width-padding*2]);
+      .range([xPadding, width-xPadding*2]);
 
   var yScale = d3.scale.ordinal()
-      .domain(data.map(function(d) { return d["call type code"]; }))
-      .rangePoints([height-padding - 10, padding])
+      .domain(data.map(function(d) { return d["call type description"]; }))
+      .rangePoints([height-yPadding - 10, yPadding])
 
   var xMap = function(d) { return xScale(xValue(d));};
 
@@ -109,14 +144,22 @@ $.get('/api/v1/stats/neigh_incident_stats' + queryString, function(data) {
 
   vis.append("g")
     .attr("class", "yaxis")
-    .attr("transform", "translate("+padding+",0)")
+    .attr("transform", "translate("+xPadding+",0)")
     .call(yAxis);
 
       // draw x axis with labels and move to the bottom of the chart area
   vis.append("g")
       .attr("class", "xaxis")
-      .attr("transform", "translate(0," + (height - padding) + ")")
-      .call(xAxis);
+      .attr("transform", "translate(0," + (height - yPadding) + ")")
+      .call(xAxis)
+      .selectAll("text")
+      .attr("y", 0)
+      .attr("x", 15)
+      .attr("dy", ".35em")
+      .attr("transform", "rotate(45)")
+      .style("text-anchor", "start")
+      .style("font-size","12px")
+
 
   vis.selectAll(".dot")
     .data(data)
@@ -124,7 +167,7 @@ $.get('/api/v1/stats/neigh_incident_stats' + queryString, function(data) {
     .append("circle")
       .attr("class", "dot")
       .attr('cx', xMap)
-      .attr('cy', function(d) { return yScale(d["call type code"]); })
+      .attr('cy', function(d) { return yScale(d["call type description"]); })
       .attr('r', 5)
       .attr('fill', 'red')
 
