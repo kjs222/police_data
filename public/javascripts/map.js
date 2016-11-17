862d0272de27179d42e5db843cbdfbf328180306
Kerry <kerry.sheldon@gmail.com>
2016-11-17 16:29:24 -0700

    neigh map rendering

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -6,8 +6,12 @@ $(document).ready(function() {
 var drawMap = function() {
   d3.json("sd_neigh_geojson.json", function(data) {
     var margin = 75,
-        width = 1920-margin,
-        height = 1080 - margin
+        width = 1000-margin,
+        height = 600 - margin
+
+    var center = d3.geo.centroid(data)
+    var scale  = 40000;
+    var offset = [width/2, height/2];
 
     var svg = d3.select("#map")
                 .append('svg')
@@ -17,21 +21,40 @@ var drawMap = function() {
                 .attr('class', 'map')
 
     var projection = d3.geo.mercator()
-                           .center([-117, 32])
-                           .scale(100)
-                           .translate([width/2, height/2])
+                           .scale(scale)
+                           .center(center)
+                           .translate(offset);
 
-    var path = d3.geo.path()
-                     .projection(projection);
+    var path = d3.geo.path().projection(projection);
 
-    var map = svg.selectAll('path')
-                 .data(data.features)
-                 .enter()
-                 .append('path')
-                 .attr('d', path)
-                 .style('fill', 'blue')
-                 .style('stroke', 'red')
+   var bounds  = path.bounds(data);
+  //  var hscale  = scale*width  / (bounds[1][0] - bounds[0][0]);
+  //  var vscale  = scale*height / (bounds[1][1] - bounds[0][1]);
+  //  var scale   = (hscale < vscale) ? hscale : vscale;
+   var offset  = [width - (bounds[0][0] + bounds[1][0])/2,
+                 height - (bounds[0][1] + bounds[1][1])/2];
 
-  })
+  projection = d3.geo.mercator()
+                     .center(center)
+                     .scale(scale)
+                     .translate(offset);
+
+  path = path.projection(projection);
+
+
+  svg.append("rect")
+     .attr('width', width)
+     .attr('height', height)
+     .style('stroke', 'black')
+     .style('fill', 'none');
+
+  svg.selectAll("path").data(data.features)
+       .enter()
+       .append("path")
+       .attr("d", path)
+       .style("fill", "red")
+       .style("stroke-width", "1")
+       .style("stroke", "black")
+   });
 
 }
