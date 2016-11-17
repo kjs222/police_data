c65fb7c3e44e7eaae33df0ca61c4b3db533da2bd
Kerry <kerry.sheldon@gmail.com>
2016-11-17 15:23:51 -0700

    rendering but very slow and small

---
+++ public/javascripts/map.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+  drawMap();
+
+});
+
+var drawMap = function() {
+  d3.json("sd_neigh_geojson.json", function(data) {
+    var margin = 75,
+        width = 1920-margin,
+        height = 1080 - margin
+
+    var svg = d3.select("#map")
+                .append('svg')
+                .attr('width', width + margin)
+                .attr('height', height + margin)
+                .append('g')
+                .attr('class', 'map')
+
+    var projection = d3.geo.mercator()
+                           .scale(1000000)
+                           .translate([width/2, height/2])
+
+    var path = d3.geo.path()
+                     .projection(projection);
+
+    var map = svg.selectAll('path')
+                 .data(data.features)
+                 .enter()
+                 .append('path')
+                 .attr('d', path)
+                 .style('fill', 'blue')
+                 .style('stroke', 'red')
+
+  })
+
+}
